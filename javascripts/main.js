419e83aa2a9c16b2a37e72233e4e9176b566616e
Dyam Marcano <dyam.marcano@gmail.com>
2016-07-09 03:27:34 -0400

    fixes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-var app = angular.module('MainApp', []);
+var app = angular.module('MainApp', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider.state('home', {
+    url:'/',
+    'content': {
+      templateUrl: 'views/homepage.html',
+    }
+  });
+
+}]);
