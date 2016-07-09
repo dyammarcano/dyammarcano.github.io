e3161f0b8644b1f3d81e57b5f00813616e1b978d
Dyam Marcano <dyam.marcano@gmail.com>
2016-07-09 04:03:45 -0400

    fixes

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,16 +2,37 @@ var app = angular.module('MainApp', ['ui.router']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/home');
 
   $stateProvider.state('home', {
-    url:'/',
-    templateUrl: 'views/homepage.html',
-    controller: 'homeController'
+    url:'/home',
+    views: {
+    	"main": {
+	    	templateUrl: 'views/homepage.html',
+	    	controller: 'homeController'
+    	}
+    }
+  });
+
+  $stateProvider.state('api', {
+    url:'/api',
+    template: 'views/api.html',
+    controller: 'apiController',
+    resolve : {
+      apiStatus: function ($http) {
+        return $http.get('../api.json').then(function (response) {
+          return response;
+        });
+      }
+    }
   });
 
 }]);
 
-app.controller('homeController', ['$scope', '$timeout', '$http', function ($scope, $timeout, $http) {
+app.controller('apiController', ['$scope', 'apiStatus' function ($scope, apiStatus) {
+	$scope.data = apiStatus.data;
+}]);
+
+app.controller('homeController', ['$scope', function ($scope) {
 	
 }]);
