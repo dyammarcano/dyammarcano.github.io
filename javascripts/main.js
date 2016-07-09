da22c5e1a20ace3c59aa8da8b97d51af036ec361
Dyam Marcano <dyam.marcano@gmail.com>
2016-07-09 04:54:48 -0400

    fixes

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,11 +7,18 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 
   $stateProvider.state('home', {
     url:'/home',
+    resolve : {
+      pageContent: function ($http) {
+        return $http.get('../content_page.json').then(function (response) {
+          return response;
+        });
+      }
+    },
     views: {
-    	"main": {
-	    	templateUrl: 'views/homepage.html',
-	    	controller: 'homeController'
-    	}
+      "main": {
+        templateUrl: 'views/homepage.html',
+        controller: 'homeController'
+      }
     }
   });
 
@@ -26,23 +33,30 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
       }
     },
     views: {
-    	"main": {
-	    	template: 'views/api.html',
-	    	controller: 'apiController',
-    	}
+      "main": {
+        templateUrl: 'views/api.html',
+        controller: 'apiController',
+      }
     }   
   });
 
 }]);
 
+
 app.controller('apiController', ['$scope', 'apiStatus', function ($scope, apiStatus) {
 
-	$scope.data = apiStatus.data;
+  $scope.data = apiStatus.data;
 
 }]);
 
 
-app.controller('homeController', ['$scope', function ($scope) {
-	
+app.controller('homeController', ['$scope', 'pageContent', function ($scope, pageContent) {
+
+  var r = pageContent.data;
+
+  $scope.title1 = r.title1;
+
+  $scope.bigName = 'Dyam Marcano';
+  console.info("Hello:", $scope.bigName);
 
 }]);
